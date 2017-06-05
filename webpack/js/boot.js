797ab9419a73ed1d7b7f9dd3c14e8ed775b6ce95
James Pearce <jpearcey1000@gmail.com>
2017-06-05 09:59:40 +0000

    Remove unrequired files

--- webpack/js/boot.js
+++
@@ -1,9 +0,0 @@
-import {bootstrap} from 'angular2/platform/browser';
-import {TodoComponent} from './TodoComponent';
-
-let boot = document.addEventListener('DOMContentLoaded', () => {
-  bootstrap(TodoComponent);
-});
-
-// Expose boot so it can be required by webpack.
-module.exports = boot;
